b10e0011cf73e9799ab06fd1086479ceb0dada7d
HIROSE Masaaki <hirose31@gmail.com>
2019-07-16 19:15:48 +0900

    Load config

--- cmd/alertist/main.go
+++ cmd/alertist/main.go
@@ -4,8 +4,12 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/user"
+
+	"gopkg.in/yaml.v2"
 
 	debug "github.com/visionmedia/go-debug"
 )
@@ -33,6 +37,9 @@ Options:
 	fmt.Printf("%#v\n", flag.Args())
 	fmt.Printf("%s\n", *configFile)
 
+	config := loadConfig()
+	_ = config // fixme
+
 	args := flag.Args()
 
 	if len(args) == 0 {
@@ -52,6 +59,7 @@ Options:
 
 func execute(args []string) (stdout string, stderr string, code int, err error) {
 	debugf("execute args: %s", args)
+
 	cmd := exec.Command(args[0], args[1:]...)
 	var out bytes.Buffer
 	var oer bytes.Buffer
@@ -60,4 +68,43 @@ func execute(args []string) (stdout string, stderr string, code int, err error) 
 	err = cmd.Run()
 
 	return out.String(), oer.String(), cmd.ProcessState.ExitCode(), err
+}
+
+func loadConfig() (config map[string]map[string]string) {
+	config = map[string]map[string]string{}
+	config["default"] = map[string]string{}
+
+	var _configFile string
+
+	if *configFile != "" {
+		_configFile = *configFile
+	} else {
+		user, _ := user.Current()
+		home := user.HomeDir
+		debugf("home: %s", home)
+
+		for _, file := range []string{"_/etc/alertist.yaml", home + "/.alertist.yaml"} {
+			debugf("exists? %s", file)
+			if _, err := os.Stat(file); err == nil {
+				_configFile = file
+				break
+			}
+		}
+	}
+	debugf("configFile: %s", _configFile)
+
+	if _configFile != "" {
+		content, err := ioutil.ReadFile(_configFile)
+		if err != nil {
+			debugf("failed to read %s: %s", _configFile, err)
+		} else {
+			err := yaml.Unmarshal(content, &config)
+			if err != nil {
+				debugf("failed to parse as yaml: %s", err)
+			}
+		}
+	}
+
+	debugf("config: %s", config)
+	return config
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hirose31/alertist
 
 go 1.12
 
-require github.com/visionmedia/go-debug v0.0.0-20180109164601-bfacf9d8a444
+require (
+	github.com/visionmedia/go-debug v0.0.0-20180109164601-bfacf9d8a444
+	gopkg.in/yaml.v2 v2.2.2
+)
